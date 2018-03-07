5cd31d52a59749cf166c8502d8361bfacdb92632
Bernard Huff <bernard.huff008@gmail.com>
2018-03-07 15:07:53 -0600

    Sweep

--- page.php
+++ page.php
@@ -6,17 +6,17 @@ while(have_posts()){
     
     the_post(); ?>
 
+    <h1>This is a page not a post</h1>
+
     <h2>
-        <a href="<?php the_permalink(); ?>">
-            <?php the_title(); ?>
-        </a>
+
+        <?php the_title(); ?>
+
     </h2>
 
     <h3>
         <?php the_content(); ?>
     </h3>
-
-    <hr>
 
     <?php
 }
--- single.php
+++ single.php
@@ -7,16 +7,14 @@ while(have_posts()){
     the_post(); ?>
 
     <h2>
-        <a href="<?php the_permalink(); ?>">
-            <?php the_title(); ?>
-        </a>
+
+        <?php the_title(); ?>
+
     </h2>
 
     <h3>
         <?php the_content(); ?>
     </h3>
-
-    <hr>
 
     <?php
 }
