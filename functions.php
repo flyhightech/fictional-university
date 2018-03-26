b6003a76bf83a799ce2fe942cd2871c9124641ea
Bernard Huff <bernard.huff008@gmail.com>
2018-03-25 19:12:11 -0500

    Disabled cache for the websites css files

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function university_files() {
     wp_enqueue_script('javascript', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
     wp_enqueue_style('google-font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+    wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, microtime());
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
