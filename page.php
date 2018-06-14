57102aa5cbe7b4cf96b96aaa2993ab5e550f7faa
Bernard Huff <bernard.huff008@gmail.com>
2018-06-13 20:41:19 -0500

    Cleaning out some content

--- page.php
+++ page.php
@@ -6,17 +6,8 @@ while(have_posts()){
     
     the_post(); ?>
 
-    <h1>This is a page not a post</h1>
 
-    <h2>
 
-        <?php the_title(); ?>
-
-    </h2>
-
-    <h3>
-        <?php the_content(); ?>
-    </h3>
 
     <?php
 }
