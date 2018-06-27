d9badd6dfbac8252bf01f08a2541579e1a4344b4
Bernard Huff <bernard.huff008@gmail.com>
2018-06-27 16:51:40 -0500

    Altered some stuff.

--- page.php
+++ page.php
@@ -22,21 +22,21 @@ while(have_posts()){
 
         <?php
 
-        $theParent = wp_get_post_parent_id(get_the_ID())
+        $theParent = wp_get_post_parent_id(get_the_ID());
 
-        if () { ?>
+        if ($theParent) { ?>
 
-        <!--       Bread crum box below.      -->
+            <!--       Bread crum box below.      -->
 
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p><a class="metabox__blog-home-link" href="<?php echo get_permalink(x); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title(x); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
-        </div>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+            </div>
 
-        <?php }
+            <?php }
 
         ?>
 
-        <!-- This is the sidebar menu
+            <!-- This is the sidebar menu
         <div class="page-links">
             <h2 class="page-links__title"><a href="#">About Us</a></h2>
             <ul class="min-list">
@@ -46,9 +46,9 @@ while(have_posts()){
         </div>
         -->
 
-        <div class="generic-content">
-            <?php the_content(); ?>
-        </div>
+            <div class="generic-content">
+                <?php the_content(); ?>
+            </div>
 
     </div>
 
