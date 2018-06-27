7bd46eb09eee900fa3d607ef7bd5aa9f50487ffb
Bernard Huff <bernard.huff008@gmail.com>
2018-06-27 16:39:52 -0500

    Comment

--- page.php
+++ page.php
@@ -20,11 +20,21 @@ while(have_posts()){
 
     <div class="container container--narrow page-section">
 
+        <?php
+
+        $theParent = wp_get_post_parent_id(get_the_ID())
+
+        if () { ?>
+
         <!--       Bread crum box below.      -->
 
         <div class="metabox metabox--position-up metabox--with-home-link">
-            <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title(); ?></span></p>
+            <p><a class="metabox__blog-home-link" href="<?php echo get_permalink(x); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title(x); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
         </div>
+
+        <?php }
+
+        ?>
 
         <!-- This is the sidebar menu
         <div class="page-links">
