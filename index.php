40ed9fb0bcd51db8f63982d3e18af7d0de521326
Bernard Huff <bernard.huff008@gmail.com>
2018-08-09 15:31:46 -0500

    Check out

--- index.php
+++ index.php
@@ -5,16 +5,20 @@ get_header();
 ?>;
 
 <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-        <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title">
-                Welcome to Our Blog
-            </h1>
-            <div class="page-banner__intro">
-                <p>Learn how the school of your dreams got started.</p>
-            </div>
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title">
+            Welcome to Our Blog
+        </h1>
+        <div class="page-banner__intro">
+            <p>Keep up with our latest news</p>
         </div>
+    </div>
 </div>;
+
+<div class="container container--narrow page-section">
+    Hello You
+</div>
 
 <?
 
