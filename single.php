e4cf39051d21f69b99ea90671d96cee227c5b8da
Bernard Huff <bernard.huff008@gmail.com>
2018-08-30 13:48:25 -0500

    Debugging the metabox.

--- single.php
+++ single.php
@@ -17,16 +17,21 @@ while(have_posts()) {
         </div>
     </div>
 
-    <div class="container container--narrow page-section"></div>
-
-    <!-- This is another metabox -->
-
-    <div class="metabox metabox--position-up metabox--with-home-link">
-        <p><a class="metabox__blog-home-link" href="<?php echo site_url(" /blog "); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home </a> <span class="metabox__main">Posted by:
+    <div class="container container--narrow page-section">
+        <div class="metabox metabox--position-up metabox--with-home-link">
+            <p><a class="metabox__blog-home-link" href="<?php echo site_url(" /blog "); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home </a> <span class="metabox__main">Posted by:
                     <?php the_author_posts_link() ;?> on
                     <?php the_time("n.j.y"); ?> in
                     <?php echo get_the_category_list(", "); ?></span></p>
+        </div>
+        <div class="generic-content">
+            <?php the_content() ;?>
+        </div>
     </div>
+
+    <!-- This is another metabox -->
+
+
 
     <?php }
 
