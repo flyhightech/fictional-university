d37cd46d7414cd73302ff4f3c39586bf81d25f5d
Bernard Huff <bernard.huff008@gmail.com>
2018-11-09 14:58:46 -0600

    Sweep

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php 
-
 get_header(); 
-
 ?>
-
 <!--           Below is the page banner         -->
-
 <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
     <div class="page-banner__content container container--narrow">
